74408fae90138ca45dcd8a26c6ce193bb1b2186e
AliShabab62 <Alishabab62@gmail.com>
2022-11-17 14:58:44 +0530

    p

--- width_change/index.js
+++ width_change/index.js
@@ -2,9 +2,9 @@ const div=document.getElementById('change_div');
 document.addEventListener('mousemove' , (event)=>{
     console.log(event);
     const width=`${event.pageX}px`
-    const height=`${event.page}px`
+    // const height=`${event.pageY}px`
 
     div.style.width=width;
-    div.style.height=height;
+    // div.style.height=height;
 
 })
